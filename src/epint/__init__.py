dd40b908b365fc88a81c0643994540d29614fc0c
muradinc <m3t3-han@hotmail.com>
2025-12-18 13:16:02 +0300

    convert MIT license to Apache 2.0

--- src/epint/__init__.py
+++ src/epint/__init__.py
@@ -1,4 +1,17 @@
 # -*- coding: utf-8 -*-
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 from .modules.version import __version__, __appname__, __fullname__, __author__
 from .modules.search.ipython_blockage import IPYTHON_MAGIC_METHODS
 from .modules.search.method_name_decorator import to_python_method_name
