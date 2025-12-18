a341db5742b708905d251c8ec22fe1fce48cee46
muradinc <m3t3-han@hotmail.com>
2025-12-18 15:33:00 +0300

    settlement_point oiz remove

--- src/epint/modules/category_proxy/__init__.py
+++ src/epint/modules/category_proxy/__init__.py
@@ -43,22 +43,27 @@ class CategoryProxy:
         
         # 1. Önce normalize edilmiş isimle tam eşleşme kontrolü
         if normalized_name in endpoints:
+            print("tam")
+            print(normalized_name)
             endpoint_data = endpoints[normalized_name]
             endpoint_name = normalized_name
         # 2. Orijinal isimle tam eşleşme kontrolü
         elif name in endpoints:
+            print("orj")
             endpoint_data = endpoints[name]
             endpoint_name = name
         else:
             # 3. Normalize edilmiş isimlerle fuzzy matching
             normalized_endpoints = {to_python_method_name(ep_name): ep_name for ep_name in endpoints.keys()}
             closest_normalized = find_closest_match(normalized_name, list(normalized_endpoints.keys()), threshold=0.7)
+            print("İlk " + closest_normalized)
             if closest_normalized:
                 endpoint_name = normalized_endpoints[closest_normalized]
                 endpoint_data = endpoints[endpoint_name]
             else:
                 # 4. Orijinal endpoint isimleriyle fuzzy matching (düşük threshold)
                 closest = find_closest_match(name, list(endpoints.keys()), threshold=0.6)
+                print("İlk " + closest)
                 if closest:
                     endpoint_name = closest
                     endpoint_data = endpoints[closest]
--- src/epint/modules/version/__init__.py
+++ src/epint/modules/version/__init__.py
@@ -14,7 +14,7 @@
 
 __major__ = 0 # stabil release
 __minor__ = 5 # yeni özellik
-__semantic__ = 0 # bug fix
+__semantic__ = 1 # bug fix
 __tag__ = "dev"
 
 __version__ = f"{__major__}.{__minor__}.{__semantic__}-{__tag__}"
